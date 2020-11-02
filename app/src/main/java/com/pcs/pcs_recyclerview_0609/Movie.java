269f5af163fc34517ce4e8582ab82d7e12ffb3f6
Nurfaizi20 <nurfaizilinggama2000@gmail.com>
2020-11-02 22:01:12 +0700

    PCS_RecyclerView_0609

---
+++ app/src/main/java/com/pcs/pcs_recyclerview_0609/Movie.java
@@ -0,0 +1,28 @@
+package com.pcs.pcs_recyclerview_0609;
+
+public class Movie {
+
+    private String title;
+    protected String year;
+
+    public Movie(String title, String year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
